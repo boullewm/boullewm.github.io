1db10c4e76f2cf647244921c11ffac976148bb7e
boullewm <boullewm@mail.uc.edu>
2024-12-05 07:17:45 -0500

    Made significant progress on each portion of the project.

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+type="text/javascript">
+        $("#submit").click(function () {
+            let name = $("#name").val();
+            let str = "Thank you " 
+                + name 
+                + " for your email!";
+            $("#modal_body").html(str);
+        });
